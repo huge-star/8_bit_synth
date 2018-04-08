1b1a8361a277178dcb572633e59816f445cffdc1
huge-star <30525493+huge-star@users.noreply.github.com>
2018-04-08 20:32:30 +0800

    大更新！做完一些啦！

---
+++ 8_bit_synth/8_bit_synth/substitude.c
@@ -0,0 +1,9 @@
+//
+//  substitude.c
+//  8_bit_synth
+//
+//  Created by 范熙来 on 2018/4/8.
+//  Copyright © 2018年 范熙来. All rights reserved.
+//
+
+#include "substitude.h"
---
+++ 8_bit_synth/8_bit_synth/substitude.h
@@ -0,0 +1,14 @@
+//
+//  substitude.h
+//  8_bit_synth
+//
+//  Created by 范熙来 on 2018/4/8.
+//  Copyright © 2018年 范熙来. All rights reserved.
+//
+
+#ifndef substitude_h
+#define substitude_h
+
+#include <stdio.h>
+
+#endif /* substitude_h */
